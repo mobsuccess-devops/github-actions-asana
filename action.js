b03f1578ed89f36fd6304c7cb3129798945a76a2
Philippe Auriach <p.auriach@gmail.com>
2023-03-22 17:54:39 +0100

    try something else

--- action.js
+++ action.js
@@ -416,6 +416,7 @@ exports.action = async function action() {
         if (shouldAssignToAsanaCreator) {
           const taskForCreator = await getTask(taskId, {
             optFields: ["created_by"],
+            opt_fields: "created_by",
           });
           console.log("taskForCreator", taskForCreator);
           updateOptions["assignee"] = taskForCreator.created_by.gid;
