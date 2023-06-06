f1729ec929d6d32d37134114c23abaa9db6b6e7b
Philippe Auriach <p.auriach@gmail.com>
2023-06-06 13:11:01 +0200

    add more log

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ async function run() {
     await action.action();
   } catch (error) {
     console.error(error);
+    if (error.message.includes("Resource not accessible by integration")) {
+      console.info(
+        `This error might be due to the Github repository settings: make sure that the checkbox "Read and write permissions" is checked here: 
+        https://github.com/mobsuccess-devops/${
+          process.env.GITHUB_REPOSITORY.split("/")[1]
+        }/settings/actions`
+      );
+    }
     core.setFailed(error.message);
   }
 }
