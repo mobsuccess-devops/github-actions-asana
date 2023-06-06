f4618e2cf955d0ba22664b4230a0a88a14c1a818
Philippe Auriach <p.auriach@gmail.com>
2023-06-06 12:53:07 +0200

    more error log

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ async function run() {
   try {
     await action.action();
   } catch (error) {
+    console.error(error);
+    console.error(error.stack);
     core.setFailed(error.message);
   }
 }
